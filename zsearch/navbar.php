f2cbb28ea5dae6d8b1134bde2b2e92af82b1b146
Rajagopal Thirugnanasambandam <76542621+rajagopal2804@users.noreply.github.com>
2022-08-07 15:35:22 +0200

    Add files via upload

---
+++ zsearch/navbar.php
@@ -0,0 +1,28 @@
+<!-- Navbar --->
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="brand" href="index.php"><img src="static/img/newlogo.png" width="170" height="50"></a>
+            <div class="nav-collapse collapse">
+                <ul class="nav alignright" style="float:right;">
+                    <li><a href="logout.php">Logout</a></li>
+                    <!--<li><a href="help.php">Help</a></li>-->
+                    <!--<li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Options<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="search_options.php">Search Options</a></li>
+                            <li class="divider"></li>
+                            <li><a href="metrics.php">Performance Metrics</a></li>-->
+
+                        </ul>
+                    </li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
+</div><!-- End of Navbar --->
